db737db6feb7a8acee155d75a39b626b16645290
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2024-09-17 11:43:00 +0300

    Update 007_range.py

--- 004_Döngüler/007_range.py
+++ 004_Döngüler/007_range.py
@@ -14,6 +14,10 @@ for i in range(3,10,2) :
 print("list(range(5,100,10))".center(50,"*"))
 print(list(range(5,100,10)))
 
+print("list(range(50,10))".center(50,"*")) # 50'den geriye sayar
+for i in range(50,10) :
+    print(i)
+
 
 
 
