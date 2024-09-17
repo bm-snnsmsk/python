44c8dda986dae4515d6a051d83534ee5e9dc5155
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2024-09-17 16:33:28 +0300

    Update 013_sets.py

--- 001_Python Objeleri ve Veri Yapıları/013_sets.py
+++ 001_Python Objeleri ve Veri Yapıları/013_sets.py
@@ -22,3 +22,11 @@ fruits.pop()  ## set listeelerinde son elemanı silmeyi garanti etmez
 print(fruits)
 
 fruits.clear()  # tüm elemanlar silinir
+
+kume1 = {1,2,3,4,5}
+kume2 = {1,2,6,9,5}
+print(kume1|kume2)   ## birleşim kümesi -  1,2,3,4,5,6,9
+print(kume2|kume1)   ## birleşim kümesi -  1,2,3,4,5,6,9
+print(kume1 & kume2)   ## kesişim kümesi -  1,2,5
+print(kume1 - kume2)   ## kume1 fark kume2 -  3,4
+print(kume2 - kume1)   ## kume2 fark kume1 -  6,9
