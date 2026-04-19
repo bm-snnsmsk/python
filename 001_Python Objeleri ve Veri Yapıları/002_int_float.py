a4e28684b7013d421080f98bd98f83f125eb6058
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-19 23:00:10 +0300

    Update 002_int_float.py

--- 001_Python Objeleri ve Veri Yapıları/002_int_float.py
+++ 001_Python Objeleri ve Veri Yapıları/002_int_float.py
@@ -8,3 +8,11 @@ sonuc2 = type(sayi2)
 
 print(sonuc1)
 print(sonuc2)
+
+print(type(12))
+print(type(12.4))
+print(type("sinan"))
+print(type("s"))
+print(type(True))
+print(type(["bir","iki",3,True,4.5]))
+print(type((1,2,"34",45)))
