91bc7633493d28ec48e59f449406f14bf7216501
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-19 22:54:58 +0300

    Update 004_degiskenler.py

--- 001_Python Objeleri ve Veri Yapıları/004_degiskenler.py
+++ 001_Python Objeleri ve Veri Yapıları/004_degiskenler.py
@@ -21,3 +21,38 @@ print(number1)
 print(number + number1)
 print(name + " " + name2)
 print(name, name2)
+
+"""
+## Uygulama 1 >>> aşağıdaki değişkenleri tanımla
+Müşteri adı
+müşteri soyadı
+müşteri adı + soyadı
+müşteri cinsiyet
+müşteri tc kimlik no
+müşteri doğum yılı
+müşteri adres bilgileri
+müşteri yaşı
+##################################
+musteriAdi = input("Adınız : ")
+musteriSoyadi = input("Soyadınız : ")
+musteriadi_soyadi = musteriAdi + " " + musteriSoyadi
+musteri_cinsiyet = input("Cinsiyetiniz : ")
+musteri_kimlikno = input("kimlik no : ")
+musteri_dogum = input("doğum yılınız : ")
+musteri_adres = input("Adresiniz : ")
+musteri_yas = input("yaşınız : ")
+print(musteriAdi+" "+musteriSoyadi)
+"""
+
+"""
+## Uygulama 2  >>> aşağıdaki siparişleri topla
+sipariş 1 = 110 TL
+sipariş 2 = 1100.5 TL
+sipariş 3 = 356.95 TL
+##################################
+siparis1 = 110
+siparis2 = 1100.5
+siparis3 = 356.95
+toplam = siparis1+siparis2+siparis3
+print(str(toplam)+" TL")
+"""
