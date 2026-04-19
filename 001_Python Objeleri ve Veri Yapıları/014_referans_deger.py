3516208cb1877680af58eae449313ac5675ac4f0
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-20 01:53:59 +0300

    Update 014_referans_deger.py

--- 001_Python Objeleri ve Veri Yapıları/014_referans_deger.py
+++ 001_Python Objeleri ve Veri Yapıları/014_referans_deger.py
@@ -1,4 +1,6 @@
 # value types => strings, number
+# value tipler değer taşır
+
 x = 5
 y = 25
 
@@ -10,11 +12,12 @@ print(y)
 
 
 # referanse types => list, class
+## referans tipler adres taşır, o yüzeden adr4steki değişkilk o adresi referans alan tüm değerleri etiler
 
 a = ["apple","banana"]
 b = ["apple","banana"]
 
-a = b
+a = b   ### list, class veriler referans tipler olduğu için adresler eşitleinir
 b[0] = "grape"  # sadece b listesinde değişiklik yapılmasına rağmen her iki liste de değişir.Çünkü Value type'lerde değer tutulur, listelerde ise address bilgisi tutulur addreler ise başka bir yerdeki değerleri işaret eder. Yani değerler eğitlendiğinde değerler değil adresler birbirne eşit olur ve bu adreslerde tutulan değerler değişince her iki liste de değişmiş olur. Amaç performans
 
 # a[0] = "çilek"
