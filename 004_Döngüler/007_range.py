daa34f637b2cf8c90e424186debf685a0825f790
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-23 00:28:05 +0300

    Update 007_range.py

--- 004_Döngüler/007_range.py
+++ 004_Döngüler/007_range.py
@@ -12,10 +12,10 @@ for i in range(3,10,2) :
     print(i)
 
 print("list(range(5,100,10))".center(50,"*"))
-print(list(range(5,100,10)))
+print(list(range(5,100,10)))      ### list(range(5,100,10)) listeye çevirme
 
 print("list(range(50,10))".center(50,"*")) # 50'den geriye sayar
-for i in range(50,10) :
+for i in range(50,10, -2) :    ### ilk sayı ikinci sayıdan büyükse eğer - bir değerle artırma yazılmalı yoksa çalışmaz
     print(i)
 
 print("list(range(50,10))".center(50,"*")) # 15'den 3'e kadar 4 azalarak geriye sayar
