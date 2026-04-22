3c56e6227b062930073e5a23636cf31bb65ae38d
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-22 23:16:23 +0300

    Update 002_while.py

--- 004_Döngüler/002_while.py
+++ 004_Döngüler/002_while.py
@@ -6,8 +6,29 @@ while x < 100 :
     x+=1
 print("while bitti...")
 
-
+#########################
 name = ""
 while not name.strip() :
     name = input("isim giriniz : ")
 print(f"adınız : {name}")
+
+
+#########################
+a = 0
+cift = 0
+tek = 0
+while True :
+    if a == 13 :
+        break
+    
+    if a % 2 == 0 :
+        cift += 1
+        print(f"{a} : çift")
+    elif a % 2 == 1 :
+        tek += 1
+        print(f"{a} : tek")
+    a += 1
+    ############################
+
+  
+print(f"çift sayısı : {cift} >>> tek sayısı : {tek}")
