ab1795e088a47f665f8973c9951ee4f89f958ee0
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-23 01:32:52 +0300

    Update 009_zip.py

--- 004_Döngüler/009_zip.py
+++ 004_Döngüler/009_zip.py
@@ -2,7 +2,7 @@ liste1 = [1,2,3,4,5]
 liste2 = ["a","b","c","d","e"]
 liste3 = ["+","-","*","/","#"]
 
-tumliste = list(zip(liste1, liste2))
+tumliste = list(zip(liste1, liste2))   #### eşleştirerek birleştirme yapar [(1,'a'),(2,'b'),(3,'c'),(4,'d'),(5,'e')]
 tumliste2 = list(zip(liste1, liste2, liste3))
 
 print(tumliste)
