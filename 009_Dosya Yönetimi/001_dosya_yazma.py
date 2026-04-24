074eb99b9a836b20b76917c6576d32056b8cf14a
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-24 07:39:07 +0300

    Update 001_dosya_yazma.py

--- 009_Dosya Yönetimi/001_dosya_yazma.py
+++ 009_Dosya Yönetimi/001_dosya_yazma.py
@@ -8,7 +8,11 @@
 # "r" okuma. dosya konumda yoksa hata verir
 
 file = open("Dosyalar/newfile.txt", "w", encoding="utf-8")
-file.write("dosyaya yazı yazıldı...")
-print(file)
-file.close()
+file.write("dosyaya yazı yazıldı...")  ## ilgili dosyaya içerik ekler
+print(file)    ### <_io.TextIOWrapper name='newfile.txt' mode='w' encoding='utf-8'>
+file.close()   ## dosya açık kalırsa kaynak tüketir, makineyi yorar. o yüzden kapatılması gerekir
+########################################3
 
+f = open("newfile.txt", "w", encoding="utf-8")
+f.write("dosyaya içerik eklendi...")  ## ilgili dosyaya içerik ekler
+f.close() 
