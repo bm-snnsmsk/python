b6128153da253143d3adbba4747e5ae54762d1f4
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-24 07:32:50 +0300

    Update 001_dosya_yazma.py

--- 009_Dosya Yönetimi/001_dosya_yazma.py
+++ 009_Dosya Yönetimi/001_dosya_yazma.py
@@ -4,8 +4,8 @@
 
 # "w"  yazma modu. dosya mevcut ise eski bilgiler silinir.
 # "a"  ekleme yapar. dosya varsa ekleme yapar dosya konumda yoksa oluşturur
-# "x"  oluşturma.dosya varsa ekleme hata verir dosya konumda yoksa oluşturur
-# "r" okuma. dosya konumda yok hata verir
+# "x"  oluşturma.dosya varsa hata verir, dosya konumda yoksa oluşturur
+# "r" okuma. dosya konumda yoksa hata verir
 
 file = open("Dosyalar/newfile.txt", "w", encoding="utf-8")
 file.write("dosyaya yazı yazıldı...")
