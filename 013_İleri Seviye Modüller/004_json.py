f6225060c2f4dacd3b96061236e960d517d2e9bb
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-04-25 23:31:14 +0300

    Update 004_json.py

--- 013_İleri Seviye Modüller/004_json.py
+++ 013_İleri Seviye Modüller/004_json.py
@@ -7,36 +7,43 @@ person = {
     "diller":["C","C++","PHP"]
 }
 
-person_string = '{"name":"Sinan", "diller":["C","C++","PHP"]}'
+person_dictionary = {"name":"Sinan", "diller":["C","C++","PHP"]}
+person_json_string = '{"name":"Sinan", "diller":["C","C++","PHP"]}'
 
 # result = person["name"]
 # result = person["diller"]
 # result = person["diller"][0]
 
 
-# json to dict
-# result = json.loads(person_string)
-# print(type(result))
+# json stringi to dict
+# result = json.loads(person_json_string)
+# print(type(result))  ## json stringi artık dictionary
 
 # result = person["name"]
 # result = person["diller"][0]
 
+
+# person.json >>> {"name":"Sinan", "diller":["C","C++","PHP"]}
 # with open("person.json") as f :
 #     data = json.load(f)
 #     print(data)
 #     print(data["name"])
 
-
+########################################################################
+person_dict = {
+    "name":"Sinan",
+    "diller":["C","C++","PHP"]
+}
 # dict to json
-result = json.dumps(person, indent=4, sort_keys=True)
-result = json.dumps(person)
+result = json.dumps(person_dict, indent=4, sort_keys=True)  ## konsolda okunurluğu düzgün olsun diye eklenen parametreler
+result = json.dumps(person_dict)   ### dictionaty artık string olduğu için person_dict["name"]  hat verecektir artık
 
 print(result) 
 print(type(result)) # str
 
 
-with open("person_2.json" ,"w") as f :
-    json.dump(person, f)
+with open("person.json" ,"w") as f :
+    json.dump(person_dict, f)
 
 
 # print(result)
