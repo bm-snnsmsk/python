04cc5a57bdc6fe26603e00d399fe1d61ff380b68
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-05-01 00:29:58 +0300

    Update 007_grid_layout.py

--- 019_PyQt5/007_grid_layout.py
+++ 019_PyQt5/007_grid_layout.py
@@ -30,6 +30,7 @@ class MainWindow(QMainWindow):
         grid_layout.addWidget(Color("yellow"), 1 ,2)  ## row, column
         grid_layout.addWidget(Color("green"), 2 ,0)   ## row, column
         grid_layout.addWidget(Color("black"), 2 ,1)    ## row, column
+        grid_layout.addWidget(Color("#f09"), 2 ,2)    ## row, column
 
         grid_layout.setContentsMargins(30, 75, 50, 30)  ## l - t - r - b 
         grid_layout.setSpacing(30)  ## 
