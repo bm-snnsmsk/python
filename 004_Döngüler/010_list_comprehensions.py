200243004ff36e64a8277ac55a33092f8525d84a
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-05-08 15:02:31 +0300

    Update 010_list_comprehensions.py

--- 004_Döngüler/010_list_comprehensions.py
+++ 004_Döngüler/010_list_comprehensions.py
@@ -21,7 +21,24 @@ print(yaslar)
 ######################################################
 sonuc = [i if i % 2 == 0 else "Tek" for i in range(1,10) ]
 print(sonuc)
+######################################################
+sonuc = ["ÇİFT" if i % 2 == 0 else "Tek" for i in range(1,10) ]
+print(sonuc)
+######################################################
+sonuc = [f"{i} : ÇİFT" if i % 2 == 0 else f"{i} : Tek" for i in range(1,10) ]
+print(sonuc)
 ############  iç içe for ########################
 sonuc1 = [(i,j) for i in range(3) for j in range(3) ]
 print(sonuc1)
 ######################################################
+sonuc = [i for i in range(1,101) if i % 3 == 0 if i % 4 == 0 ] ## 12'e bölünnen sayılar
+print(sonuc)
+######################################################
+ogrenciler = ["ali","ahmet","canan"]
+notlar = [50,75,62]
+liste = [(ogrenciler[i], notlar[i]) for i in range (0, len(ogrenciler))]
+print(liste)
+liste_dict = {key:value for (key, value) in liste}
+print(liste_dict)
+liste_dict = {key:value for (key, value) in liste if value > 50}
+print(liste_dict)
