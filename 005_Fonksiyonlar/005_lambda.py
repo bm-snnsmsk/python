0cbac7e677a79490c8cca1548616176eefc81071
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-05-08 15:06:04 +0300

    Update 005_lambda.py

--- 005_Fonksiyonlar/005_lambda.py
+++ 005_Fonksiyonlar/005_lambda.py
@@ -2,6 +2,13 @@ def kareal(sayi) : return sayi ** 2   ## ## lambda sayi: sayi**2    = kareal = l
 
 print(kareal(5))
 
+
+kupAl = lambda i : i**3
+kupAl(10)
+sonuc = (lambda i : i**3)(10)
+print(sonuc)
+
+
 sayilar = [1,2,3,4,5]
 
 ########## map ###################
@@ -24,3 +31,22 @@ print(list(map(f, liste)))
 ########## lambda ################
 elveda = lambda : print("Güle güle")
 elveda()
+
+###################################333
+sayilar222 = ["1","2","3","4","5"]
+sonuc = list(map(int, sayilar222))  ## int strleri integere cevirir
+print(sonuc)   
+
+###################################333
+isimler = ["ali","berfin","dilek","baran","tuba"]
+sonuc = list(map(str.capitalize, isimler))  ## 
+print(sonuc)   
+
+###################################333
+users = [
+    {"ad":"ali","soyad":"sönmez"},
+    {"ad":"ahmet","soyad":"kartal"},
+    {"ad":"dilek","soyad":"güneş"}
+]
+sonuc = list(map(lambda i : i["ad"], users))  ## 
+print(sonuc) 
