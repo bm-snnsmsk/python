44245b22a6470791b8862cc3b43ef167bc48920e
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-08-14 21:27:21 +0300

    Update 003_math_operatorleri.py

--- 001_Python Objeleri ve Veri Yapıları/003_math_operatorleri.py
+++ 001_Python Objeleri ve Veri Yapıları/003_math_operatorleri.py
@@ -6,7 +6,10 @@
 # **     üs alma
 # %      mod alma - bölümden kalan
 
-print(10/3)
-print(10//3)
-print(2**8)
-print(11%3)
+print(10/3)     >>> 3.3333333
+print(10//3)    >>> 3
+print(10**3)    >>> 1000
+print(12%3)     >>> 0
+print(2**4)
+print(10//3)   >>> 3
+print(-10//3)  >>> -4
