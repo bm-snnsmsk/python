ca86657520794ac80c902c3ad204e1f57914ec13
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-08-15 00:23:00 +0300

    Update 004_demo_2.py

--- 005_Fonksiyonlar/004_demo_2.py
+++ 005_Fonksiyonlar/004_demo_2.py
@@ -1,8 +1,21 @@
 def tambolenler(sayi) :
     tambolenler = []
-    for i in range(2, sayi):
+    for i in range(2, sayi + i):
         if sayi % i == 0 :
             tambolenler.append(i)
     return tambolenler
 
 print(tambolenler(20))
+
+
+"""
+def bolenler(sayi) :
+    tambolenler = []
+    for i in range(1, sayi + 1) :
+        if sayi % i == 0 :
+            tambolenler.append(i)
+            print(f"{sayi}, {i}'ye tam bölünüyor...")
+        else :
+            print(f"{sayi}, {i}'ye bölünmüyor...")
+    return tambolenler
+"""
