247da597f1adf0cf7187f28d952d428d8b1f33b9
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-08-28 15:15:05 +0300

    Update 002_numpy.py

--- 015_numpy/002_numpy.py
+++ 015_numpy/002_numpy.py
@@ -3,10 +3,11 @@ import numpy as np
 result = np.array([1,3,5,7,9,11])
 result = np.array([1,3,5,7,9,11]).reshape(3,2)   ### eleman sayısı sutun X satır sçarpımını karşılamalı
 result = np.arange(1,10)  ## 1 den başla 10' a kadar
+result = np.arange(1,10).reshape(3,3)   ### eleman sayısı sutun X satır sçarpımını karşılamalı  ## 1 den başla 10' a kadar
 result = np.arange(10,100, 3) # 10 dan başla 100' e akdar 3 atlayarak
-result = np.zeros(10) ## 10  sıfır float 
+result = np.zeros(10).reshape(5,2)   ### eleman sayısı sutun X satır sçarpımını karşılamalı ## 10  sıfır float 
 result = np.ones(10)  ## 10 bir float
-result = np.linspace(0, 100, 5)  ## aralığı 5 e böl
+result = np.linspace(0, 100, 5)  ## aralığı 5 e böl >>> [  0.  25.  50.  75. 100.]
 result = np.linspace(0, 100, 5)  ## aralığı 5 e böl
 result = np.random.randint(0, 10)  ## 0-10 aralığında rastgele bir sayı
 result = np.random.randint(0, 10, 3)  ## 0-10 aralığında rastgele 3 sayı
