4f7ebfe76cd171d6b260a9454001f9b361d4ef18
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-08-28 15:27:27 +0300

    Update 002_numpy.py

--- 015_numpy/002_numpy.py
+++ 015_numpy/002_numpy.py
@@ -9,8 +9,26 @@ result = np.zeros(10).reshape(5,2)   ### eleman sayısı sutun X satır sçarpı
 result = np.ones(10)  ## 10 bir float
 result = np.linspace(0, 100, 5)  ## aralığı 5 e böl 4 eşit aralıpğa böl >>> [  0.  25.  50.  75. 100.]
 result = np.linspace(0, 5, 8)  ## [0.  0.71428571 1.42857143 2.14285714 2.85714286 3.57142857 4.28571429 5. ]   >>> 8 nokta
+
+
 result = np.random.randint(0, 10)  ## 0-10 aralığında rastgele bir sayı
-result = np.random.randint(0, 10, 3)  ## 0-10 aralığında rastgele 3 sayı
+
+result = np.random.randint(0, 5, 1000)  ## 0-5 aralığında rastgele 1000 sayı
+liste = []
+for i in range(10000) :
+    liste.append(np.random.randint(0, 5))
+print(liste.count(0))
+print(liste.count(1))
+print(liste.count(2))
+print(liste.count(3))
+print(liste.count(4))
+print(liste.count(5))
+
+print(liste) >>>> [1, 3, 3, 1, 0, 4, 3, 3, 3, 1, 1, 2, 1, 0, 0, 2, 4, 3, 4, 3, 3, 1, 2, 3, 4, 4, 2, 4, 3, 0, 1, 1, 1, 3, 3, 1, 1, 0, 3, 2, 4, 4, 4, 3, 1, 3, 4, 3, 4, 4, 1, 3, 0, 2, 2, 0, 3, 0, 3, 4, 1, 1, 0, 0, 0, 0, 3, 2, 2, 1, 3, 3, 3, 1, 2, 0, 1, 4, 0, 0, 4, 2, 1, 4, 0, 2, 3, 3, 4, 1, 2, 2, 0, 3, 1, 0, 3, 0, 4, 3]
+print(result) >>> [2 4 2 0 0 1 0 4 0 3 4 0 4 0 3 4 3 4 3 0 0 2 4 0 0 2 1 0 2 3 3 1 4 4 0 2 2
+ 0 3 3 3 4 4 1 0 2 3 0 0 0 0 1 4 3 4 1 0 0 0 2 0 2 0 2 3 4 1 3 2 1 2 0 4 3
+ 4 4 3 0 2 1 4 4 2 3 3 2 2 3 4 3 4 4 3 3 4 3 0 3 0 0]
+
 result = np.random.rand(5)  ## 0-1 aralığında rastgele 5 sayı
 result = np.random.randn(5)  ## 0-1 aralığında rastgele 5 negatif sayı
 result = np.random.randn(5)  ## 0-1 aralığında rastgele 5 negatif sayı
