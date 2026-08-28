0c73d9241dc5c0bdb5664ec3bc3321334571a53b
Sinan Şimşek <100801415+bm-snnsmsk@users.noreply.github.com>
2026-08-28 14:57:51 +0300

    Update 001_numpy.py

--- 015_numpy/001_numpy.py
+++ 015_numpy/001_numpy.py
@@ -5,8 +5,8 @@ py_list = [1,2,3,4,5,6,7,8,9]
 # numpy array
 np_array = np.array([1,2,3,4,5,6,7,8,9])
 
-# print(type(py_list))
-# print(type(np_array))
+# print(type(py_list))     ### <class 'list'>
+# print(type(np_array))    ### <class 'numpy.ndarray'>
 
 py_multi = [[1,2,3], [4,5,6], [7,8,9]]
 np_multi = np_array.reshape(3,3)
